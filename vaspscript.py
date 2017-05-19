cbcf76284cc4bacd7461a8e33a26286cd222287f
smccaffrey <smccaffrey70@gmail.com>
2017-05-18 23:14:19 -0700

    cleaned up

--- vaspscript.py
+++ vaspscript.py
@@ -1,31 +1,26 @@
 import os
+import shutil
+
+
 os.system('mkdir testingenv')
 
-import os
 os.system('phonopy diamond.conf -d')
 
-import os
-import shutil
 source = os.listdir('/Users/andrewadams/desktop/vasp')
 destination = 'testingenv'
 for files in source:
     if files.startswith('POSCAR'):
         shutil.move(source, destination)
 
-import os
-import shutil
+
 source = os.listdir('/Users/andrewadams/desktop/vasp')
 destination = 'testingenv'
 for files in source:
     if files('disp.yaml'):
         shutil.move(source, destination)
 
-import os
 os.system('cd /Users/andrewadams/desktop/vasp')
 
-import os
 os.system('phonopy -f disp-001/vasprun.xml disp-002/vasprun.xml')
 
-import os
 os.system('phonopy -f disp-001/vasprun.xml disp-002/vasprun.xml')
-
